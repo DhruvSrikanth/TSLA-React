f64602e8585338ef7641a47bf8a5be76e532aa0b
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-03-23 03:09:48 -0500

    updated app import

--- src/app.py
+++ src/app.py
@@ -3,8 +3,8 @@ from collections import deque
 
 import dash
 from dash.dependencies import Output, Input
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 
 import plotly
 import plotly.graph_objs as go
